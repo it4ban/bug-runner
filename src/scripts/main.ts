374803b6d5579d1bc8389ed7e88faa550dc3a3eb
Alex Yagodkin <adamsHillzzz@gmail.com>
2024-11-14 12:15:57 +0300

    feat: add hover animation

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,10 @@
-import '../css/style.css';
+import '../css/style.scss';
+
+const header = document.querySelector('.header');
+const bubble = document.querySelector('.bubble') as HTMLElement;
+const bubbleImg = bubble?.querySelector('.bubble__img') as HTMLElement;
+
+header?.addEventListener('mousemove', (e) => {
+	bubble.style.transform = `translate3d(${e.pageX}px, ${e.pageY}px, 0)`;
+	bubbleImg.style.transform = `translate3d(${-e.pageX}px, ${-e.pageY}px, 0)`;
+});
