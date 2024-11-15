8d280498d8da37346d3bccc46ccd1c31ada74707
Alex Yagodkin <adamsHillzzz@gmail.com>
2024-11-15 14:10:05 +0300

    refactor: replace animation for moving bug

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,48 +1,58 @@
 import lottie from 'lottie-web';
+import { gsap } from 'gsap';
+import { MotionPathPlugin } from 'gsap/all';
 
 import '../css/style.scss';
 
 const header = document.querySelector('.header');
 const bubble = document.querySelector('.bubble') as HTMLElement;
 const bubbleImg = bubble?.querySelector('.bubble__img') as HTMLElement;
-const bug = document.querySelector('.bug') as HTMLElement;
-const bugContainer = document.querySelector('.bugs-top');
+
+let lottieAnimation = lottie.loadAnimation({
+	container: document.querySelector('.bugs-top') ?? document.body,
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: '/bug-runner/src/data/bug-animation.json',
+});
+
+const headerReact = header?.getBoundingClientRect();
+
+gsap.registerPlugin(MotionPathPlugin);
+gsap.config({
+	autoSleep: 320,
+	force3D: true,
+	nullTargetWarn: true,
+	units: {
+		lineHeight: '',
+	},
+});
+
+const animation = gsap.to('.bugs-top', {
+	motionPath: {
+		path: [
+			{ x: 0, y: 0 },
+			{ x: headerReact?.right, y: headerReact?.bottom + 300 },
+		],
+		autoRotate: true,
+	},
+	transform: 10,
+	transformOrigin: '50% 50%',
+	duration: 5,
+	overwrite: false,
+	delay: 0,
+	repeat: -1,
+});
 
 let targetX = 0;
 let targetY = 0;
 let currentX = 0;
 let currentY = 0;
 
-const headerRect = header?.getBoundingClientRect();
-const diagonalPositions = [
-	{ x: headerRect?.left, y: headerRect?.top },
-	{ x: headerRect?.right, y: headerRect?.bottom },
-	{ x: headerRect?.right, y: headerRect?.top },
-	{ x: headerRect?.left, y: headerRect?.bottom },
-];
-
-let currentPositionIndex = 0;
-let startX = diagonalPositions[0].x;
-let startY = diagonalPositions[0].y;
-let endX = diagonalPositions[1].x;
-let endY = diagonalPositions[1].y;
-let progress = 0;
-const speed = 0.005;
-
 header?.addEventListener('mousemove', function (e: Event) {
 	const mouseEvent = e as MouseEvent;
 	targetX = mouseEvent.pageX;
 	targetY = mouseEvent.pageY;
-
-	console.log(targetX, targetY);
-});
-
-lottie.loadAnimation({
-	container: document.querySelector('.bug') ?? document.body,
-	renderer: 'svg',
-	loop: true,
-	autoplay: true,
-	path: '/bug-runner/src/data/bug-animation.json',
 });
 
 function animateHover() {
@@ -54,27 +64,5 @@ function animateHover() {
 
 	requestAnimationFrame(animateHover);
 }
-
-function animateBug() {
-	progress += speed;
-
-	const currentX = startX + (endX - startX) * progress;
-	const currentY = startY + (endY - startY) * progress;
-
-	bug.style.transform = `matrix(1, 0, 0, 1, ${currentX}, ${currentY})`;
-
-	if (progress >= 1) {
-		progress = 0;
-		currentPositionIndex = (currentPositionIndex + 1) % diagonalPositions.length;
-		startX = diagonalPositions[currentPositionIndex].x;
-		startY = diagonalPositions[currentPositionIndex].y;
-		endX = diagonalPositions[(currentPositionIndex + 1) % diagonalPositions.length].x;
-		endY = diagonalPositions[(currentPositionIndex + 1) % diagonalPositions.length].y;
-	}
-
-	requestAnimationFrame(animateBug);
-}
-
-animateBug();
 
 animateHover();
