d5c53d05f4121e3261926f9f7995bd02c1e294d3
Alex Yagodkin <adamsHillzzz@gmail.com>
2024-11-18 15:44:41 +0300

    feat: add custome cursor

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,8 @@
-import { BugAnimation } from './core';
-
 import '../css/style.scss';
 
+import gsap from 'gsap';
+
+import { BugAnimation } from './core';
 import { CockraochName } from './enums';
 import { ITransform } from './interface';
 
@@ -9,6 +10,36 @@ const header = document.querySelector<HTMLElement>('.header');
 const bubble = document.querySelector<HTMLElement>('.bubble');
 const bubbleImg = bubble?.querySelector<HTMLElement>('.bubble__img');
 const bugsTop = document.querySelector<HTMLElement>('.bugs-top');
+
+const pulseTimeline = gsap.timeline({ repeat: -1, yoyo: true });
+
+pulseTimeline
+	.to('.cursor span', {
+		scale: 1.3,
+		duration: 0.6,
+		ease: 'power1.inOut',
+	})
+	.to('.cursor span', {
+		scaleX: 1.3,
+		scaleY: 0.8,
+		duration: 0.6,
+		ease: 'power1.inOut',
+	});
+
+gsap.set('.cursor', {
+	xPercent: -50,
+	yPercent: -50,
+});
+
+let xTo = gsap.quickTo('.cursor', 'x', { duration: 0.01, ease: 'power1.inOut' });
+let yTo = gsap.quickTo('.cursor', 'y', { duration: 0.01, ease: 'power1.inOut' });
+
+window.addEventListener('mousemove', (e) => {
+	const mouseEvent = e as MouseEvent;
+
+	xTo(mouseEvent.clientX);
+	yTo(mouseEvent.clientY);
+});
 
 const updatePaths: Partial<Record<CockraochName, ITransform>> = {
 	'cockraoch control': {
